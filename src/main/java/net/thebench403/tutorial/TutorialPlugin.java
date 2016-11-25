bb26c25079da83c6e59912ef2b2f4e96e3b48e14
Corin22225 <mrcoltj@gmail.com>
2016-11-25 14:04:40 -0500

    Initial commit

---
+++ src/main/java/net/thebench403/tutorial/TutorialPlugin.java
@@ -0,0 +1,25 @@
+package net.thebench403.tutorial;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TutorialPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if(!player.isOp()) {
+            return;
+        }
+        player.sendMessage(ChatColor.GOLD + "This works.");
+    }
+}
